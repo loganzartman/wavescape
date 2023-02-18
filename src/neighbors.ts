c903d25228eec66668e7ec7d4f8ae3725db41ae1
nondefault <spam@nondefault.net>
2023-02-17 18:45:37 -0800

    implement (broken) neighbor algorithm on cpu
    the problem is that I initially assumed that it would be ok for there to be hash collisions; as that would just result in traversing a superset of the intended neighbors. However, it is NOT ok because, if two cells hash to the same value, that means that any particles in those cells will be visited TWICE; once per each cell. Ultimately this leads to extra forces being accumulated. Instead, I think I need to give up and use a fixed-size spatial domain so that I can have a perfect cell position hash.

--- src/neighbors.ts
+++ src/neighbors.ts
@@ -2,8 +2,11 @@ import {State} from './state';
 import {Params} from './params';
 
 const neighborTable = new Map<number, number[]>();
+const keyParticlePairs: Array<[number, number]> = [];
+const startCount: Array<[number, number]> = [];
 
-const cellKey = (i: number, j: number) => (73856093 * i) ^ (19349663 * j);
+const cellKey = (i: number, j: number) =>
+  Math.abs((73856093 * i) ^ (19349663 * j));
 
 const posToCell = (params: Params, x: number, y: number): [number, number] => {
   const neighborCellSize = 1.0 * params.hSmoothing;
@@ -11,6 +14,59 @@ const posToCell = (params: Params, x: number, y: number): [number, number] => {
 };
 
 export const updateNeighbors = (state: State, params: Params) => {
+  keyParticlePairs.length = 0;
+  for (let i = 0; i < state.n; ++i) {
+    const cellPos = posToCell(
+      params,
+      state.position[i * 2 + 0],
+      state.position[i * 2 + 1]
+    );
+    const key = cellKey(cellPos[0], cellPos[1]) % state.n;
+    keyParticlePairs.push([key, i]);
+  }
+
+  keyParticlePairs.sort((a, b) => a[0] - b[0]);
+
+  startCount.length = 0;
+  for (let i = 0; i < state.n; ++i) {
+    startCount.push([state.n, 0]);
+  }
+
+  for (let i = 0; i < state.n; ++i) {
+    const [key, _] = keyParticlePairs[i];
+    startCount[key][0] = Math.min(startCount[key][0], i);
+    startCount[key][1] += 1;
+  }
+};
+
+export const forEachNeighbor = (
+  state: State,
+  params: Params,
+  index: number,
+  fn: (i: number) => void
+) => {
+  const [cx0, cy0] = posToCell(
+    params,
+    state.position[index * 2 + 0] - params.hSmoothing,
+    state.position[index * 2 + 1] - params.hSmoothing
+  );
+  const [cx1, cy1] = posToCell(
+    params,
+    state.position[index * 2 + 0] + params.hSmoothing,
+    state.position[index * 2 + 1] + params.hSmoothing
+  );
+  for (let cx = cx0; cx <= cx1; ++cx) {
+    for (let cy = cy0; cy <= cy1; ++cy) {
+      const key = cellKey(cx, cy) % state.n;
+      const [start, count] = startCount[key];
+      for (let i = start; i < start + count; ++i) {
+        fn(keyParticlePairs[i][1]);
+      }
+    }
+  }
+};
+
+export const updateNeighborsx = (state: State, params: Params) => {
   neighborTable.clear();
   for (let i = 0; i < state.n; ++i) {
     const [cellX, cellY] = posToCell(
@@ -26,7 +82,7 @@ export const updateNeighbors = (state: State, params: Params) => {
   }
 };
 
-export const forEachNeighbor = (
+export const forEachNeighborx = (
   state: State,
   params: Params,
   index: number,
